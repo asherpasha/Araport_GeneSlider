f66321d415ae9d9fd34e4a06c4c30fe255b52601
Asher <asher.pasha@utoronto.ca>
2015-05-17 01:33:36 -0400

    Added some JS code.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,13 @@
-(function(window, $, undefined) {
+/*globals Processing*/
+(function(window, $, Processing, undefined) {
 	'use strict';
 	
 	// Global variables
 	//var appContext = $('[data-app-name="araport-geneslider"]');
+	var pjs;	// Processing js object
+	var bound;	// If Processing is bound
+	var jsonClone;	// This will have data for gff
+	var search;	// The JSON object for search
 
 	// Functions
 
@@ -25,7 +30,122 @@
 		}
 	}
 
+	// Bind processing
+	function bindjs() {
+		pjs = Processing.getInstanceById('araport-geneslider-canvas');
+		if (pjs !== null) {
+			pjs.bindJavascript(this);
+			bound = true;
+		}
+		if (!bound) { 
+			setTimeout(bindjs, 250);
+		}
+	}
 
-	// Run the script
-	loadPDE();
-})(window, jQuery);
+
+	// Load an AGI into GeneSlider
+	function agiLoader(agi, before, after, zoomFrom, zoomTo, Bitscore, alnIndicator) {
+		var Agave = window.Agave;
+		var query;
+		var response;
+	
+		function bind() {
+			pjs = Processing.getInstanceById('araport-geneslider-canvas');
+		
+			if (pjs !== null) {
+				pjs.bindJavascript(this);
+				bound = true;
+	
+				// Runs the webservice that fetches data using AGI
+				function addData() {
+					query = {
+						locus: agi,
+						before: before,
+						after: after
+					};
+
+					Agave.api.adama.search({
+						'namespace':'asher', 'service':'araport_geneslider_alignmentbyagi_v0.1.0', 'queryParam': query
+					}, function(reponse) {
+						// Check for server errors
+						if (response.status !== 200) {
+							window.alert('Error in backend webservice!');
+							return;
+						}
+
+						// Check for error from webservice
+						if (response.obj.status === 'failed') {
+							window.alert('An error returned by webservice!');
+							return;
+						}
+
+						jsonClone = JSON.parse(JSON.stringify(reponse.obj.result));
+						if (reponse.obj.result.fileData !== '') {
+							pjs.resetData();
+							pjs.setAlnStart(response.obj.result.start);
+							pjs.setSessionData('CNSData', agi, before, after, Bitscore, alnIndicator);
+	
+							// Set the start digit
+							if (zoomFrom < 0) {
+								pjs.setStartDigit(before);
+							} else {
+								pjs.setStartDigit(zoomFrom);
+							}
+	
+							// Set the end digit
+							if (zoomTo < 0) {
+								pjs.setEndDigit(before + 20);
+							} else {
+								pjs.setEndDigit(zoomTo);
+							}
+	
+							
+							pjs.setgffPanelOpen(true);
+							pjs.setFastaData(response.obj.result.fileData);
+							
+							// Set the search query
+							if (search.length > 0 && search.length < 7) {
+								for (var i = 0; i < search.length; i++) {
+									for (var j = 0; j < search[i].length; j++) {
+										// Set in Processing goes here
+										for (var key in search[i][j]) {
+											pjs.setSearch(parseInt(i,10), parseInt(j, 10), key, parseFloat(search[i][j][key]));
+										}
+									}
+								}
+							}
+							setTimeout(pjs.updateURLSearchResults, 100);	
+						}
+					});
+				}
+				addData();
+			}
+			if (!bound) {
+				setTimeout(bind, 250);
+			} 
+		}
+		bind();
+	}
+
+	$(document).ready(function() {
+		// Variable
+		var agi = 'At1g01010';
+		var before = 1000;
+		var after = 1000;
+		var zoomFrom = -1;
+		var zoomTo = -1;
+		var weightedBitscore = 'true';
+		var alnIndicator = 'true';
+
+
+		// Run the script
+		loadPDE();
+
+		// Bind
+		bindjs();
+
+		// Load AGI
+		agiLoader(agi, parseInt(before, 10), parseInt(after, 10), parseInt(zoomFrom, 10), parseInt(zoomTo, 10), weightedBitscore, alnIndicator);
+	});
+
+})(window, jQuery, Processing);
