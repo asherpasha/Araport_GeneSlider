15c7e1710d0f12388c2348948cb89e75ef5fbaf7
Asher <asher.pasha@utoronto.ca>
2015-05-20 16:15:47 -0400

    Added two buttons.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -32,10 +32,77 @@ var jsonClone;	// This will have data for gff
 		}
 	}
 
+	// This function check the validity of the fasta file.
+	// then be analized in JavaScript or Processing.
+	function checkFastaSeq(fileData) {
+		// Variables
+		var fileDataArray;	// This array will hold the data from the user file
+		var seqLength;	// The length of sequences in the file
+	
+		// RegEx patterns
+		var startPattern = /^>/;
+		var digits = /\d/;
+		// Special character * is now allowed
+		var special = /\[|\]|\^|\$|\.|\||\?|\+|\(|\)|~|\`|\!|\@|\#|\%|\&|\_|\+|\=|\{|\}|\'|\"|<|\>|\:|\;|\,/;
+		// Sequence X is allow allowed!
+		var seq = /b|j|o|u|z/i;
+	
+		// Converts big string into arrays
+		fileDataArray = fileData.split('\n');
+	
+		// Checks for ">" in the first line first sentence
+		if (!(startPattern.test(fileDataArray[0]))) {
+			window.alert('This is not a FASTA file.');
+			return false;
+		}
+	
+		// Check for number of lines
+		if (fileDataArray.length <= 2) {
+			window.alert('Atleast 2 seqeunces are needed.');
+			return false;
+		}
+		
+		// Check for sequence lengths
+		// lengths of all sequences must be the same
+		seqLength = fileDataArray[1].length;	// Get the length of the first sequence
+		for (var i=0; i < fileDataArray.length - 1; i++) {
+			if (!(startPattern.test(fileDataArray[i]))) {
+				// Get the length of the sequence and check it against the length of first sequence
+				var length = fileDataArray[i].length;
+				if (length !== seqLength) {
+					window.alert('The lengths of the sequences are not equal.');
+					return false;
+				}
+	
+				// Check for digits
+				if (digits.test(fileDataArray[i])) {
+					window.alert('The sequences should not have digits.');
+					return false;
+				}
+	
+				// Check for DNA/Protein Sequence
+				if (seq.test(fileDataArray[i])) {
+					window.alert('The file has non DNA/Protein sequence.');	
+					return false;
+				}
+
+				// Check for special character
+				if (special.test(fileDataArray[i])) {
+					window.alert('The sequence has special characters.');
+					return false;
+				}
+			}
+		}
+		
+		// All tests passed
+		return true;
+	}
+
 	// Bind processing
 	function bindjs() {
 		pjs = Processing.getInstanceById('araport-geneslider-canvas');
 		if (pjs) {
+			/*jshint validthis:true */
 			pjs.bindJavascript(this);
 			bound = true;
 		}
@@ -47,7 +114,6 @@ var jsonClone;	// This will have data for gff
 	// Load an AGI into GeneSlider
 	function agiLoader(agi, before, after, zoomFrom, zoomTo, Bitscore, alnIndicator) {
 		var query;
-		var response;
 	
 		function bind() {
 			pjs = Processing.getInstanceById('araport-geneslider-canvas');
@@ -118,6 +184,7 @@ var jsonClone;	// This will have data for gff
 			}
 
 			if (pjs) {
+				/*jshint validthis:true */
 				pjs.bindJavascript(this);
 				bound = true;
 
@@ -128,6 +195,46 @@ var jsonClone;	// This will have data for gff
 			} 
 		}
 		bind();
+	}
+
+	// Read file from user
+	function readFile(evt) {
+		var file = evt.files[0];
+	
+		// Check Processing
+		if (!bound) {
+			bindjs();
+		}
+	
+		// Load the user supplied file
+		if (file) {
+			var inputFile = new FileReader();
+			var contents;	// The contents of the file as one big string
+			
+			inputFile.onload = function(e) {
+				contents = e.target.result;	// Get the file contents
+	
+				// Fix FASTA files with multiple lines
+				var components = contents.split('>');
+				var parts;
+				components.shift();
+				for (var i = 0; i < components.length; i++) {
+					parts = components[i].split(/\n|\r/);
+					parts[0] = '>' + parts[0] + '\n';
+					components[i] = parts.join('');
+					components[i] = components[i].toUpperCase();
+					components[i] = components[i] + '\n';
+				}
+				contents = components.join('');
+				
+				// Check validity of FASTA data, and send it to GeneSlider
+				if (checkFastaSeq(contents)) {
+					pjs.resetData();
+					pjs.setFastaData(contents);	// Set fasta data
+				}
+			};
+			inputFile.readAsText(file);
+		}
 	}
 
 	// Load data
@@ -143,7 +250,7 @@ var jsonClone;	// This will have data for gff
 		if (ready) {
 			agiLoader(agi, parseInt(before, 10), parseInt(after, 10), parseInt(zoomFrom, 10), parseInt(zoomTo, 10), weightedBitscore, alnIndicator);
 		} else {
-			alert('Agave not ready. Please try again.');
+			window.alert('Agave not ready. Please try again.');
 		}
 	}
 
@@ -158,17 +265,35 @@ var jsonClone;	// This will have data for gff
 	window.addEventListener('Agave::ready', function() {
 		ready = true;	// Agave ready
 		$(document).ready(function() {
+			// Go Button to load alignment from AGI
 			$(document).on('click', '#araport-geneslider-go', loadDataFromGoButton);
+
+			// Downstream slider
 			$('#araport-geneslider-afterSlider').change(function() {
 				var newVal = this.value;
 		    	document.getElementById('araport-geneslider-afterlabel').innerHTML='After ('+newVal+')';
 				after = Math.abs(newVal);
 			});
 
+			// Upstream slider
 			$('#araport-geneslider-beforeSlider').change(function() {
 				var newVal = this.value;
 		    	document.getElementById('araport-geneslider-beforelabel').innerHTML='Before ('+newVal+')';
 				before = Math.abs(newVal);		
+			});
+
+			// FASTA file upload
+			$('#araport-geneslider-fileUpload').change(function () {
+			    if ($('#araport-geneslider-fileUpload').val() === '') {
+        			return;
+    			}
+				readFile(this);
+    			$('#araport-geneslider-fileUpload').val('');
+			});
+			
+			// About
+			$(document).on('click', '#araport-geneslider-about', function() {
+				window.alert('Araport GeneSlider was developed by the BAR team with support from the Araport team. GeneSlider is funded by Genome Canada.');
 			});
 		});
 	});
